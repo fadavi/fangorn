83f0ebbfa35adaf8db9dd4c89b2468b3912f65cf
Mohamad Fadavi <fadavimohamad@gmail.com>
2022-11-26 22:29:57 +0300

    fix: ceil the result

--- fangorn/seed/encode_int.py
+++ fangorn/seed/encode_int.py
@@ -1,7 +1,8 @@
+from math import ceil
 from base64 import b85encode
 
 
 def encode_int(num: int) -> str:
-    n_len = (num.bit_length() + 7) // 8
+    n_len = ceil((num.bit_length() + 7) / 8)
     n_bytes = num.to_bytes(n_len, byteorder='little', signed=True)
     return b85encode(n_bytes).decode().strip('=')
